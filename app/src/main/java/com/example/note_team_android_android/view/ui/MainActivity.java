460bdc661f627eef386fb4678d46cbba68f25e0e
Jaspreet Kaur <jassaulakh.00@gmail.com>
2021-06-16 16:11:31 -0400

    create note and card view layout done

---
+++ app/src/main/java/com/example/note_team_android_android/view/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.note_team_android_android.view.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.note_team_android_android.R;
+
+public class MainActivity  extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
